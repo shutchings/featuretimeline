1ea3b89653efca5ee034e37e6949438991c7a105
Steven Hutchings <sthutchi@microsoft.com>
2019-06-27 09:13:20 -0700

    Add some styling for teams and projects list on directory cards

--- src/PortfolioPlanning/Components/Directory/PlanCard.tsx
+++ src/PortfolioPlanning/Components/Directory/PlanCard.tsx
@@ -16,8 +16,16 @@ export default class PlanCard extends React.Component<PlanCardProps> {
             <div className="plan-card-container" onClick={() => this.props.onClick(this.props.id)}>
                 <Card className="plan-card">
                     <div className="flex-column">
-                        <div className="title-s name">{this.props.name}</div>
-                        <div className="body-m description">{this.props.description}</div>
+                        <div className="name">{this.props.name}</div>
+                        <div className="description">{this.props.description}</div>
+                        <div className="teams-container">
+                            <div className="teams-label">Teams</div>
+                            <div>Fabrikam, Contoso, WIT X</div>
+                        </div>
+                        <div className="projects-container">
+                            <div className="projects-label">Projects</div>
+                            <div>Fabrikam, AzureDevOps</div>
+                        </div>
                     </div>
                 </Card>
             </div>
