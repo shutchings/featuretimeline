9dbf49dd15dff83172a0dc98d3b7894b42b3146c
Steven Hutchings <sthutchi@microsoft.com>
2019-06-27 11:01:54 -0700

    Make card a functional component

--- src/PortfolioPlanning/Components/Directory/PlanCard.tsx
+++ src/PortfolioPlanning/Components/Directory/PlanCard.tsx
@@ -3,32 +3,32 @@ import { Card } from "azure-devops-ui/Card";
 
 import "./PlanCard.scss";
 
-export interface PlanCardProps {
+export interface IPlanCardProps {
     id: string;
     name: string;
     description: string;
+    teams: string[];
+    projects: string[];
     onClick: (id: string) => void;
 }
 
-export default class PlanCard extends React.Component<PlanCardProps> {
-    public render() {
-        return (
-            <div className="plan-card-container" onClick={() => this.props.onClick(this.props.id)}>
-                <Card className="plan-card">
-                    <div className="flex-column">
-                        <div className="name">{this.props.name}</div>
-                        <div className="description">{this.props.description}</div>
-                        <div className="teams-container">
-                            <div className="teams-label">Teams</div>
-                            <div>TODO</div>
-                        </div>
-                        <div className="projects-container">
-                            <div className="projects-label">Projects</div>
-                            <div>TODO</div>
-                        </div>
+export const PlanCard = (props: IPlanCardProps) => {
+    return (
+        <div className="plan-card-container" onClick={() => props.onClick(props.id)}>
+            <Card className="plan-card">
+                <div className="flex-column">
+                    <div className="name">{props.name}</div>
+                    <div className="description">{props.description}</div>
+                    <div className="teams-container">
+                        <div className="teams-label">Teams</div>
+                        <div>{props.teams.join(", ")}</div>
                     </div>
-                </Card>
-            </div>
-        );
-    }
-}
+                    <div className="projects-container">
+                        <div className="projects-label">Projects</div>
+                        <div>{props.projects.join(", ")}</div>
+                    </div>
+                </div>
+            </Card>
+        </div>
+    );
+};
