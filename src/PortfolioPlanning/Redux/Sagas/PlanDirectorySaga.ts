e138add09ff6c67160f99e41bceff5a075f2c74a
Steven Hutchings <sthutchi@microsoft.com>
2019-06-27 17:56:11 -0700

    Remove commented code

--- src/PortfolioPlanning/Redux/Sagas/PlanDirectorySaga.ts
+++ src/PortfolioPlanning/Redux/Sagas/PlanDirectorySaga.ts
@@ -30,23 +30,6 @@ export function* deletePlan(
     yield effects.call([service, service.DeletePortfolioPlan], id);
 }
 
-// export function* updateProjectsAndTeamsMetadata(
-//     action: ActionsOfType<PlanDirectoryActions, PlanDirectoryActionTypes.UpdateProjectsAndTeamsMetadata>
-// ): SagaIterator {
-//     const { id, projectNames, teamNames } = action.payload;
-
-//     const service = PortfolioPlanningDataService.getInstance();
-
-//     const planToUpdate: PortfolioPlanningMetadata = yield effects.call(
-//         [service, service.GetPortfolioPlanDirectoryEntry],
-//         id
-//     );
-//     planToUpdate.projectNames = projectNames;
-//     planToUpdate.teamNames = teamNames;
-
-//     yield effects.call([service, service.UpdatePortfolioPlanDirectoryEntry], planToUpdate);
-// }
-
 export function* updateProjectsAndTeamsMetadata(
     action: ActionsOfType<EpicTimelineActions, EpicTimelineActionTypes.PortfolioItemsReceived>
 ): SagaIterator {
