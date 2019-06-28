f920dc71fd5ac07a79db10ba05064a97f0d9f070
Steven Hutchings <sthutchi@microsoft.com>
2019-06-28 14:52:36 -0700

    Delete feature timeline content

--- src/FeatureTimeline/redux/store/teamiterations/types.ts
+++
@@ -1,6 +0,0 @@
-import { TeamSettingsIteration } from "TFS/Work/Contracts";
-
-export interface ITeamSettingsIterationState {
-    // project -> team -> Backlog Configuration
-    teamSettingsIterations: IDictionaryStringTo<IDictionaryStringTo<TeamSettingsIteration[]>>;
-}
