41733b98cc2e2a40fca14d87eba78658c4d9718a
Steven Hutchings <sthutchi@microsoft.com>
2019-07-10 10:37:08 -0700

    Rename effort on data model

--- src/PortfolioPlanning/Redux/Reducers/EpicTimelineReducer.ts
+++ src/PortfolioPlanning/Redux/Reducers/EpicTimelineReducer.ts
@@ -191,9 +191,9 @@ function handlePortfolioItemsReceived(
                     endDate: item.TargetDate,
                     completedCount: item.CompletedCount,
                     totalCount: item.TotalCount,
-                    completedEffort: item.CompletedStoryPoints,
-                    totalEffort: item.TotalStoryPoints,
-                    effortProgress: item.StoryPointsProgress,
+                    completedEffort: item.CompletedEffort,
+                    totalEffort: item.TotalEffort,
+                    effortProgress: item.EffortProgress,
                     countProgress: item.CountProgress
                 };
             });
@@ -257,9 +257,9 @@ function handlePortfolioItemsReceived(
                         endDate: newItemInfo.TargetDate,
                         completedCount: newItemInfo.CompletedCount,
                         totalCount: newItemInfo.TotalCount,
-                        completedEffort: newItemInfo.CompletedStoryPoints,
-                        totalEffort: newItemInfo.TotalStoryPoints,
-                        effortProgress: newItemInfo.StoryPointsProgress,
+                        completedEffort: newItemInfo.CompletedEffort,
+                        totalEffort: newItemInfo.TotalEffort,
+                        effortProgress: newItemInfo.EffortProgress,
                         countProgress: newItemInfo.CountProgress
                     });
 
